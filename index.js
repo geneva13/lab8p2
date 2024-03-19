6da8f3ae7ef076a2a21600542377ce7581da6a1f
Geneva Jacobs <144170448+geneva13@users.noreply.github.com>
2024-03-19 00:06:06 +0000

    first

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add JavaScript below
+
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+const supabaseUrl = 'https://bulkffxplgttqshuifys.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1bGtmZnhwbGd0dHFzaHVpZnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NzYyMjMsImV4cCI6MjAyNjM1MjIyM30.8zvPt_sRI68lEu5l-_1IllS10bO4xbfWKaXMRcZhqEQ'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+
+async function getBooks() {
+  let { data: books, error } = await supabase
+    .from('books')
+    .select('*')
+
+  for (let book of books) {
+     let bookList = document.getElementById('books');
+     bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.publisher}</td></tr>`;
+   }
+}
+
+getBooks();
